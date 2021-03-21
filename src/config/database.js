189e4dda7f694b7086ef9a2efb434928f51ba6a0
Ricardo B Oliveira <ricardoboliveira@MacBook-Air-de-Ricardo.local>
2021-03-21 16:30:43 -0300

    variavel de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
-    host: process.env.CLEARDB_DATABASE_URL,
-    // host: "http://187.45.196.222", IP do Banco de Dados
+   // host: process.env.CLEARDB_DATABASE_URL,
+   // host: "http://187.45.196.222", IP do Banco de Dados
+    host: process.env.MYSQL_URL,
     dialect: "mysql",
     username: "valorize_db",
     password: "db@valorize*07",
