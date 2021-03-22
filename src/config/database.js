61d78c6b8bf8b0e05a46ea1cab879f4f536e3b18
Ricardo B Oliveira <ricardoboliveira@MacBook-Air-de-Ricardo.local>
2021-03-22 05:36:22 -0300

    alterando host

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 module.exports = {
    // host: process.env.CLEARDB_DATABASE_URL,
    // host: "http://187.45.196.222", IP do Banco de Dados
-    host: process.env.CLEARDB_DATABASE_URL,
+   // host: us-cdbr-east-03.cleardb.com
+   //mysql://baa6f81fbb6f42:116d857a@us-cdbr-east-03.cleardb.com/heroku_56fabe11fc4acdd?reconnect=true
+    // host: process.env.CLEARDB_DATABASE_URL,
+    host: "us-cdbr-east-03.cleardb.com",
     dialect: "mysql",
     username: "baa6f81fbb6f42",
     password: "116d857a",
