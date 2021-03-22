011b0720c5210b7b1e89269dceb780c227099f8f
Ricardo B Oliveira <ricardoboliveira@MacBook-Air-de-Ricardo.local>
2021-03-22 09:45:39 -0300

    alterando banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,21 @@
+// module.exports = {
+//     host: "us-cdbr-east-03.cleardb.com",
+//     dialect: "mysql",
+//     username: "baa6f81fbb6f42",
+//     password: "116d857a",
+//     database: "heroku_56fabe11fc4acdd",
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//     },
+// }
+
 module.exports = {
-   // host: process.env.CLEARDB_DATABASE_URL,
-   // host: "http://187.45.196.222", IP do Banco de Dados
-   // host: us-cdbr-east-03.cleardb.com
-   //mysql://baa6f81fbb6f42:116d857a@us-cdbr-east-03.cleardb.com/heroku_56fabe11fc4acdd?reconnect=true
-    // host: process.env.CLEARDB_DATABASE_URL,
-    host: "us-cdbr-east-03.cleardb.com",
+    host: "valorize_db.mysql.dbaas.com.br",
     dialect: "mysql",
-    username: "baa6f81fbb6f42",
-    password: "116d857a",
-    database: "heroku_56fabe11fc4acdd",
+    username: "valorize_db",
+    password: "db@valorize*07",
+    database: "valorize_db",
     define: {
         timestamps: true,
         underscored: true,
