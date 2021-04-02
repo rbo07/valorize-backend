3e79263f51f0ff30501262c3e96d3ddb79dd6d96
Ricardo B Oliveira <ricardoboliveira@MacBook-Air-de-Ricardo.local>
2021-04-02 08:44:24 -0300

    variavel de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,9 @@
-// module.exports = {
-//     host: "us-cdbr-east-03.cleardb.com",
-//     dialect: "mysql",
-//     username: "baa6f81fbb6f42",
-//     password: "116d857a",
-//     database: "heroku_56fabe11fc4acdd",
-//     define: {
-//         timestamps: true,
-//         underscored: true,
-//     },
-// }
-
 module.exports = {
-    host: "valorize_db.mysql.dbaas.com.br",
+    host: process.env.HOST_NAME,
     dialect: "mysql",
-    username: "valorize_db",
-    password: "db@valorize*07",
-    database: "valorize_db",
+    username: process.env.USER_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     define: {
         timestamps: true,
         underscored: true,
